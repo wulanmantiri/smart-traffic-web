fc7928c69c1de69416a3bf17a86c83f6c83d141a
Wulan Mantiri <wulan.mantiri@gmail.com>
2021-09-13 16:48:29 +0800

    Handle responsiveness

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,29 @@ import reportWebVitals from './reportWebVitals';
 
 import Routes from 'routes';
 
-ReactDOM.render(<Routes />, document.getElementById('root'));
+ReactDOM.render(
+  <>
+    <div className="flex xl:hidden">
+      <nav className="bg-squid fixed w-screen">
+        <div className="flex items-center justify-center h-12 px-4 sm:px-12">
+          <div className="flex flex-col items-center">
+            <p className="text-white text-sm">Smart Traffic Lights</p>
+            <p className="text-gray-300 text-xs">(Web Version)</p>
+          </div>
+        </div>
+      </nav>
+      <div className="flex flex-col justify-center items-center h-screen w-screen p-4">
+        <p className="text-4xl text-center text-gray-700 font-medium">
+          Please open in full desktop screen.
+        </p>
+      </div>
+    </div>
+    <div className="hidden xl:block">
+      <Routes />
+    </div>
+  </>,
+  document.getElementById('root'),
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
